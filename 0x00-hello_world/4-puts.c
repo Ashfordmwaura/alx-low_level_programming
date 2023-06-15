7313341bc98e2258fd38aac06537676d2d3092cf
Ashfordmwaura <ashfordkamau.m@gmail.com>
2023-06-15 09:17:50 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * Description: main - Prints out a string to stdout.
- * Return: 0 if success.
+ * main - Entry
+ * Return: Always 0
  */
 int main(void)
 {
-puts("\"Programming is like building a multiligual puzzle");
+        puts("\"Programming is like building a multiligual puzzle");
 return (0);
 }
